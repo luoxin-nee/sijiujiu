f5247cff5aa2d61c0bbea1b02cc9c6fd0f07c6b1
luoxin-nee <56818317+luoxin-nee@users.noreply.github.com>
2019-11-04 09:08:32 +0800

    Create jingyu.js

---
+++ jingyu.js
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>鲸鱼</title>
+    <link rel="stylesheet" href="index.css">
+
+<body>
+    <div class="header width_all">
+        <ul class="link">
+            <li><a href="index.html">首页</a></li>
+            <li><a href="jingyu.html">鲸鱼</a></li>
+            <li><a href="aboutUs.html">关于我们</a></li>
+        </ul>
+        <div class="logo">
+            <a href="#" title="鲸鱼">
+                <img src="img/鲸鱼.png" alt="鲸鱼">
+                <span>鲸鱼的日常</span>
+            </a>
+        </div>
+     <p>最新动态&nbsp&nbsp&nbsp <span>WHAT'S <span style="color: red;font-size: 38px;">NEW!!!</span></span></p>
+    </div>
+  <!--   新闻列表 -->
+  <div id="content">
+    
+    <ul class="nl">
+        <div class="loading">
+            <div class="pic">
+                <i></i>
+                <i></i>
+                <i></i>
+                <i></i>
+                <i></i>
+            </div>
+        </div>
+    </ul>
+    <div class="clean"></div>
+</div>
+   <!--  分页 -->
+    <ul id="pagelist">
+        <li><a href="#" id="page1">1</a></li>
+        <li><a href="#" id="page2">2</a></li>
+        <div class="clean"></div>
+    </ul>
+    <!-- 底部 -->
+     <div class="footer">
+        <div class="footer_top">
+            <div class="footer1"><img src="img/电话.png" alt=""><p>800-820-8820</p></div>
+            <div class="footer2"><img src="img/短信.png" alt=""><p>1313133@163.com</p></div>
+            <div class="footer3"><img src="img/地图.png" alt=""><p>护鲸团总部</p></div>
+            <div class="footer4"><img src="img/二维码.png" alt="">扫一扫</div>
+        </div>
+        <div class="footer_bottom">
+            <hr>
+            <h4>版权所有&四九九啊</h4>
+        </div>
+    </div>
+</body>
+
+<script src="jquery-1.12.4.js"></script>
+<script src="jingyu.js"></script>
+ 
+</html>
